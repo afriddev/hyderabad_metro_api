cde4a4505cbe28b0904119bf422e97d12279ddf6
SHIAK AFRID <afridayan01@gmail.com>
2024-12-27 10:10:37 +0000

    a

--- app/connectDB.py
+++ app/connectDB.py
@@ -1,6 +1,6 @@
 from databases import Database
 
 
-DATABASE_URL = "postgres://neondb_owner:7Eazfni3Uomg@ep-winter-sound-a5vrl30y-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:7Eazfni3Uomg@ep-winter-sound-a5vrl30y.us-east-2.aws.neon.tech/neondb?sslmode=require"
 database = Database(
     DATABASE_URL)
