e6c222277f2420d86ed88934172fd703835d5001
SHIAK AFRID <afridayan01@gmail.com>
2025-03-20 09:47:33 +0000

    updated middleware code

--- app/middleWare.py
+++ app/middleWare.py
@@ -1,14 +1,24 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request
 from fastapi.responses import JSONResponse
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 class Custom404Middleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        response = await call_next(request)
-        if response.status_code == 404:
+        try:
+            logging.info(f"Processing request: {request.method} {request.url}")
+            response = await call_next(request)
+            if response.status_code == 404:
+                return JSONResponse(
+                    status_code=404,
+                    content={"message": "Wrong URL, please check the route and try again."},
+                )
+            return response
+        except Exception as e:
+            logging.error(f"Middleware Error: {e}")
             return JSONResponse(
-                status_code=404,
-                content={"message": "Wrong URL, please check the route and try again."},
+                status_code=500,
+                content={"message": "Internal Server Error"},
             )
-        return response
