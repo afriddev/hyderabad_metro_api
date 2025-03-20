89ac462b2b072d628f7ddaafdc16c2631fe491f3
SHIAK AFRID <afridayan01@gmail.com>
2025-03-20 09:55:20 +0000

    aa

--- app/middleWare.py
+++ app/middleWare.py
@@ -1,14 +1,11 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request
 from fastapi.responses import JSONResponse
-import logging
-
-logging.basicConfig(level=logging.INFO)
+import traceback
 
 class Custom404Middleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         try:
-            logging.info(f"Processing request: {request.method} {request.url}")
             response = await call_next(request)
             if response.status_code == 404:
                 return JSONResponse(
@@ -16,9 +13,13 @@ class Custom404Middleware(BaseHTTPMiddleware):
                     content={"message": "Wrong URL, please check the route and try again."},
                 )
             return response
+        except AssertionError as e:
+            return JSONResponse(
+                status_code=400,
+                content={"error": "AssertionError", "message": str(e), "trace": traceback.format_exc()},
+            )
         except Exception as e:
-            logging.error(f"Middleware Error: {e}")
             return JSONResponse(
                 status_code=500,
-                content={"message": "Internal Server Error"},
+                content={"error": "Internal Server Error", "message": str(e), "trace": traceback.format_exc()},
             )
